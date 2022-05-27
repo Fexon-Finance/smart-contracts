91e364922748a34af57357e64995ce0f3993f665
Orthae <orthaefluorite@gmail.com>
2022-05-27 14:47:27 +0200

    rip

--- contracts/FexonCore.sol
+++ contracts/FexonCore.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "./interfaces/IFexonTradeAlgoritm.sol";
 import "./interfaces/IFexonOracle.sol";
 import "./structures/Coin.sol";
+import "./structures/TradeData.sol";
 
 import "../dependencies/pancake-smart-contracts/projects/exchange-protocol/contracts/interfaces/IPancakeRouter02.sol";
 import "../dependencies/pancake-smart-contracts/projects/exchange-protocol/contracts/interfaces/IPancakeFactory.sol";
@@ -28,29 +29,40 @@ contract FexonCore is ERC20, Ownable, KeeperCompatibleInterface {
         string memory tokenName,
         string memory tokenSymbol
     ) ERC20(tokenName, tokenSymbol) {
-        require(coins[0].coinAddress == _BUSD, "First coin must be BUSD.");
         _pancakeRouter = IPancakeRouter02(pancakeRouter);
         _fexonTradeAlgorithm = IFexonTradeAlgorithm(fexonTradeAlgorithm);
         _fexonOracle = IFexonOracle(fexonOracle);
         for(uint256 i = 0; i < coins.length; i++) {
             _coins.push(Coin(coins[i].symbol, coins[i].coinAddress));    
         }
-        _mint(msg.sender, 1 * (10**18));
     }
 
-    function buy(uint256 busdAmount) public {
-        IERC20(_BUSD).transferFrom(msg.sender, address(this), busdAmount);
-        uint256 etfTokens = busdAmount / getCurrentPrice();
-        _mint(msg.sender, etfTokens);
+    function buy() public payable {
+        uint256 etf = msg.value * (10 ** 8) / getCurrentPrice();
+        require(etf > 0, "Not enough BNB for ETF token");
+
+        TradeData[] memory tradeData = _fexonTradeAlgorithm.getRatios(_buildPortfolio());
+        for(uint i = 0; i < tradeData.length ; i++) {
+            _buyCoin(tradeData[i]);
+        }
+       
+        _mint(msg.sender, etf);
     }
 
     function sell(uint256 amount) public {
         uint256 callerBalance = balanceOf(msg.sender);
         require(amount <= callerBalance, "Insufficient balance");
-        uint256 withdrawAmount = amount * getCurrentPrice();
-        IERC20(_BUSD).transfer(msg.sender, withdrawAmount);
+        uint256 ratio = ((callerBalance * 100) / totalSupply());
+
+        for(uint i = 0; i < _coins.length ; i++) {
+            uint256 coinLimit = (IERC20(_coins[i].coinAddress).balanceOf(address(this)) * ratio) / 100;
+            uint256 callerRatio = (amount * 100) / callerBalance;
+            uint256 sellAmount = (coinLimit * callerRatio) / 100;
+            _sellCoin(sellAmount, _coins[i].coinAddress);
+        }
+
         _burn(msg.sender, amount);
-    }
+        }
 
     function viewPortfolio() public view returns (PortfolioEntry[] memory) {
         PortfolioEntry[] memory portfolio = new PortfolioEntry[](_coins.length);
@@ -78,12 +90,16 @@ contract FexonCore is ERC20, Ownable, KeeperCompatibleInterface {
     function performUpkeep(bytes calldata performData) override public {}
 
     function getCurrentPrice() public view returns (uint256) {
+        if(totalSupply() == 0) {
+            return 10**8;
+        }
+
         uint256 cumulativeValue = 0;
         for(uint256 i = 0; i < _coins.length; i++) {
             IERC20 coin = IERC20(_coins[i].coinAddress);
-            cumulativeValue += uint256(_fexonOracle.getCurrentPrice(address(coin))) * coin.balanceOf(address(this));
+            cumulativeValue += uint256(_fexonOracle.getCurrentPrice(address(coin))) * coin.balanceOf(address(this)) / 1 ether;
         }
-        return cumulativeValue  / totalSupply();
+        return cumulativeValue / totalSupply();
     }
 
     function _buyCoin(TradeData memory data) private {
@@ -105,7 +121,7 @@ contract FexonCore is ERC20, Ownable, KeeperCompatibleInterface {
         pool[1] = _WBNB;
 
         IERC20(coin).approve(address(_pancakeRouter), amount);
-        _pancakeRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        _pancakeRouter.swapExactTokensForETH(
             amount,
             0,
             pool,
